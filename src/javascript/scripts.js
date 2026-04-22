63d9ab956a04202cae4461d71898f3fde23c12a6
Vitória Vasconcelos <vitoria.mvsv@gmail.com>
2026-04-21 21:12:34 -0300

    deixei o carrinho de compras funcional

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -2,6 +2,15 @@ const cart = document.getElementById("cart");
 const openCart = document.getElementById("open");
 const closeCart = document.getElementById("close");
 
+const cartTitle = document.querySelector(".cart h2");
+const cartItemsContainer = document.querySelector(".cart-items");
+const cartFooter = document.querySelector(".cart-footer");
+const cartTotal = document.querySelector(".cart-total");
+const emptyCartImg = document.querySelector("#empty-cart");
+const emptyCartMsg = document.querySelector(".cart p");
+
+const confirmBtn = document.querySelector(".confirm-btn")
+
 openCart.addEventListener("click", () => {
   cart.style.display = "block";
 });
@@ -30,4 +39,88 @@ window.addEventListener("scroll", () => {
       link.classList.add("ativo");
     }
   });
+});
+
+//carrinho
+let carrinho = [];
+
+openCart.addEventListener("click", () => {
+  cart.style.display = "block";
+});
+
+closeCart.addEventListener("click", () => {
+  cart.style.display = "none";
+});
+
+
+function atualizarCarrinho() {
+  cartItemsContainer.innerHTML = "";
+  let total = 0;
+
+  if (carrinho.length === 0) {
+    emptyCartImg.style.display = "block";
+    emptyCartMsg.style.display = "block";
+    cartFooter.style.display = "none";
+    cartTitle.textContent = "Itens no carrinho (0)";
+    return;
+  }
+
+  emptyCartImg.style.display = "none";
+  emptyCartMsg.style.display = "none";
+  cartFooter.style.display = "block";
+  cartTitle.textContent = `Itens no carrinho (${carrinho.length})`;
+
+  carrinho.forEach((item, index) => {
+    total += item.price;
+
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.textContent = "Excluir";
+
+    deleteBtn.addEventListener("click", () => {
+      carrinho.splice(index, 1);
+      atualizarCarrinho();
+    });
+
+    li.textContent = `${item.name} - R$ ${item.price.toFixed(2)} `;
+    li.appendChild(deleteBtn);
+    cartItemsContainer.appendChild(li);
+  });
+
+
+  cartTotal.textContent = `Total: R$ ${total.toFixed(2)}`;
+}
+
+document.querySelectorAll(".add-btn").forEach(button => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const productContainer = button.closest(".box");
+    const productName = productContainer.querySelector("h3").textContent;
+
+    const precoTexto =
+      productContainer.querySelector(".preco").childNodes[0].textContent;
+
+    const productPrice = parseFloat(
+      precoTexto.replace("R$", "").replace(",", ".").trim()
+    );
+
+    carrinho.push({
+      name: productName,
+      price: productPrice
+    });
+
+    atualizarCarrinho();
+  });
+});
+
+confirmBtn.addEventListener("click", () => {
+  if (carrinho.length === 0) return;
+
+  alert("Agradecemos seu pedido!");
+
+  carrinho = [];
+  atualizarCarrinho();
+  cart.style.display = "none";
 });
